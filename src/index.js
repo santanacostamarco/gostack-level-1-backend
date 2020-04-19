95ab48d61d6fade352a7607311be2c52596ac949
Marco Santana <santannacostamarco@gmail.com>
2020-04-19 20:05:19 -0300

    routes setted up

--- src/index.js
+++ src/index.js
@@ -1,31 +1,70 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 const app = express();
 
 app.use(express.json());
 
+const projects = [];
+
 app.get('/projects', (request, response) => {
-  return response.json([
-    'project 1',
-    'project 2'
-  ])
+  const { title, owner } = request.query;
+
+  const result = title || owner
+    ? projects.filter(project => (
+      project.title.includes(title) || project.owner.includes(owner)
+    ))
+    : projects
+
+  return response.json(result);
 })
+
 app.post('/projects', (request, response) => {
-  return response.json([
-    'project 1',
-    'project 2'
-  ])
+  const { title, owner } = request.body;
+  
+  if (!title || !owner) {
+    return response.status(400).json({ error: 'Title and owner is required.' });
+  }
+  
+  const project = {
+    id: uuid(),
+    title,
+    owner
+  };
+  projects.push(project);
+
+  return response.json(project);
+
 })
 app.put('/projects/:id', (request, response) => {
-  return response.json([
-    'project 1',
-    'project 2'
-  ])
+  const { id } = request.params;
+  const { title, owner } = request.body;
+  const projectIndex = projects.findIndex(project => (project.id === id))
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.' })
+  }
+
+  const project = {
+    id, 
+    title: title ? title : projects[projectIndex].title, 
+    owner: owner ? owner : projects[projectIndex].owner
+  }
+
+  projects[projectIndex] = project;
+
+  return response.json(projects[projectIndex]);
+
 })
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    'project 1',
-    'project 2'
-  ])
+  const { id } = request.params;
+  const projectIndex = projects.findIndex(project => (project.id === id))
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.' })
+  }
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 })
 
 app.listen(3333, () => {
